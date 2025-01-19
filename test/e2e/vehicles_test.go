57696352c36f34c9b6e789e26fd1338fd5e34db6
Igor Karpukhin <karpukhinigor@gmail.com>
2025-01-19 21:24:18 +0100

    Added vehicles

---
+++ test/e2e/vehicles_test.go
@@ -0,0 +1,14 @@
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/igor-karpukhin/gh-labels-test/pkg/vehicles"
+)
+
+var _ = Describe("Users test", Label("test-users"), func() {
+	It("should create a new user", func() {
+		Expect(vehicles.NewCar("BMW", "525")).NotTo(BeNil())
+	})
+})
